39526b0462164f98bb1585d36d5ac274c13f77d2
aosagula <alejandro.sagula@gmail.com>
2026-08-31 18:05:28 -0300

    pixel_id first_contact

--- src/components/landing/first-contact-page.tsx
+++ src/components/landing/first-contact-page.tsx
@@ -3,7 +3,10 @@
 import { useEffect, useRef } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import Script from "next/script";
 import { WHATSAPP_URL, INSTAGRAM_URL } from "@/lib/content";
+
+const FIRST_CONTACT_META_PIXEL_ID = "1655722448999268";
 
 declare global {
   interface Window {
@@ -114,6 +117,31 @@ const improveExamples = [
 export function FirstContactPage() {
   return (
     <>
+      <Script id="meta-pixel-first-contact" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '${FIRST_CONTACT_META_PIXEL_ID}');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src={`https://www.facebook.com/tr?id=${FIRST_CONTACT_META_PIXEL_ID}&ev=PageView&noscript=1`}
+          alt=""
+        />
+      </noscript>
+
       <header className="nav fc-nav">
         <Link className="logo" href="/" aria-label="agentic4biz inicio">
           <Image src="/logotipo_transparente.png" alt="Agentic4Biz" width={2172} height={724} priority />
